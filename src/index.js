a30d228c74061643ef6530551ed697467b9b576a
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-01-06 02:42:11 +0300

    index + reset.css

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const root = document.querySelector('.root');
-
-const Handlebars = require("handlebars");
-const template = Handlebars.compile("Name: <span>{{firstname}}</span> {{lastname}}");
-root.innerHTML = template({ firstname: "Vlad", lastname: "Burnaev" });
