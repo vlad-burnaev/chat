513f64e9cad66900835375ef8aef3f387834e0b7
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-01-06 02:20:24 +0300

    SignUp refactoring

--- static/SignUp/SignUp.js
+++ static/SignUp/SignUp.js
@@ -2,6 +2,7 @@ import {FormFieldTemplate} from "../../src/components/FormField/FormField.templa
 import {ButtonTemplate} from "../../src/components/Button/Button.template";
 import {SignUpData} from "./SignUp.data";
 import {signUpTemplate} from "./SignUp.template";
+import {FormTemplate} from "../../src/components/Form/Form.template";
 
 const Handlebars = require("handlebars");
 
@@ -9,7 +10,8 @@ const container = document.querySelector('.signUp');
 
 const FormField = Handlebars.compile(FormFieldTemplate);
 const Button = Handlebars.compile(ButtonTemplate);
-Handlebars.registerPartial({ FormField, Button });
+const Form = Handlebars.compile(FormTemplate);
+Handlebars.registerPartial({ Form, FormField, Button });
 
 const SignUpPage = Handlebars.compile(signUpTemplate);
 container.innerHTML = SignUpPage(SignUpData);
--- static/SignUp/SignUp.template.js
+++ static/SignUp/SignUp.template.js
@@ -1,14 +1,3 @@
 export const signUpTemplate = `
-    <div class="modal">
-        <h1 class="title">{{title}}</h1>
-        <form class="form">
-            {{#formfields}}
-                {{> FormField }}
-            {{/formfields}}
-            <div class="submitButtonWrapper">
-                {{> Button submitButton }}
-            </div>
-            <a href="../SignIn/SignIn.html" class="secondaryButton">Войти</a>
-        </form>
-    </div>
+    {{> Form form}}
 `
