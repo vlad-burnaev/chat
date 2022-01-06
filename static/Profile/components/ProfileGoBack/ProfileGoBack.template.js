b09072bbd8339d142849f3c61aecb1b64c31cc0d
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-01-06 04:30:12 +0300

    Profile: templanization

---
+++ static/Profile/components/ProfileGoBack/ProfileGoBack.template.js
@@ -0,0 +1,7 @@
+export const ProfileGoBackTemplate = `
+    <a href={{href}} class="goBack">
+        <div class="goBackButton">
+            <img src={{image.src}} class="goBackButtonIcon" alt={{image.alt}} />
+        </div>
+    </a>
+`
